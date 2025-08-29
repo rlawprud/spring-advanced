7d2088845ed4287e8ea39275d465703386e06889
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-08-29 17:03:29 +0900

    필수 lv1 해결 (ArgumentResolver 등록)

---
+++ src/main/java/org/example/expert/config/WebMvcConfiguration.java
@@ -0,0 +1,18 @@
+package org.example.expert.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+// ArgumentResolver를 등록하기 위한 클래스입니다.
+// WebMvcConfigurer 인터페이스를 상속받아 등록합니다.
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthUserArgumentResolver());
+    }
+}
